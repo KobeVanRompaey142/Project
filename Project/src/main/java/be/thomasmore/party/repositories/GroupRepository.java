4a5fdefea8ba0520025da042f0767f217ec33d39
KobeVanRompaey142 <75724254+KobeVanRompaey142@users.noreply.github.com>
2021-03-30 14:49:51 +0200

    Delete Project directory

--- Project/src/main/java/be/thomasmore/party/repositories/GroupRepository.java
+++
@@ -1,7 +0,0 @@
-package be.thomasmore.party.repositories;
-
-import be.thomasmore.party.model.Groupp;
-import org.springframework.data.repository.CrudRepository;
-
-public interface GroupRepository extends CrudRepository<Groupp, Integer> {
-}
